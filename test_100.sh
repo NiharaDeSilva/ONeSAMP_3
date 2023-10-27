d9312e6df7be43d83d3fe3c317b7798105e1168b
nihara <suhashi.desilva@gmail.com>
2023-10-26 22:13:47 -0400

    add and update shell scripts

--- test_100.sh
+++ test_100.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 #SBATCH --job-name=oneSamp    # Job name
 #SBATCH --mail-type=ALL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
-#SBATCH --mail-user=yu.hong@ufl.edu     # Where to send mail
-#SBATCH --ntasks=1                    # Run on a single CPU
+#SBATCH --mail-user=suhashi.desilva@gmail.com     # Where to send mail
+#SBATCH --ntasks=5                    # Run on a single CPU
 #SBATCH --mem=1gb                     # Job memory request
 #SBATCH --time=24:00:00               # Time limit hrs:min:sec
 #SBATCH --output=serial_test_%j.log   # Standard output and error log
 pwd; hostname; date
 
 module load R/4.1
-chmod 777 /blue/boucher/yu.hong/OneSampTest/build/OneSamp
+chmod +rwx /blue/boucher/suhashidesilva/ONeSAMP_3/build/OneSamp
 
-echo "Running plot script on a single CPU core"
+echo "Running plot script on multiple CPU cores"
 
-python /blue/boucher/yu.hong/oneSampTest/main.py --s 20000 --o /blue/boucher/yu.hong/oneSampTest/DataSet/genePop100Ix100L > /blue/boucher/yu.hong/oneSampTest/genePop100Ix100L.out
+python /blue/boucher/suhashidesilva/ONeSAMP_3/main.py --s 20000 --o /blue/boucher/suhashidesilva/ONeSAMP_3/DataSet/genePop100Ix100L > /blue/boucher/suhashidesilva/ONeSAMP_3/genePop100Ix100L.out
 
 date
