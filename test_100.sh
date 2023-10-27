5a06feaacbb40367f1d6b1a87be085aa67499ac6
nihara <suhashi.desilva@gmail.com>
2023-10-26 23:28:21 -0400

    update script file

--- test_100.sh
+++ test_100.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=oneSamp    # Job name
 #SBATCH --mail-type=ALL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
-#SBATCH --mail-user=yu.hong@ufl.edu     # Where to send mail
+#SBATCH --mail-user=suhashi.desilva@gmail.com     # Where to send mail
 #SBATCH --ntasks=1                    # Run on a single CPU
 #SBATCH --mem=1gb                     # Job memory request
 #SBATCH --time=24:00:00               # Time limit hrs:min:sec
@@ -9,10 +9,10 @@
 pwd; hostname; date
 
 module load R/4.1
-chmod +rwx /blue/boucher/yu.hong/OneSampTest/build/OneSamp
+chmod +rwx /blue/boucher/suhashidesilva/ONeSAMP_3/build/OneSamp
 
 echo "Running plot script on a single CPU core"
 
-python /blue/boucher/yu.hong/oneSampTest/main.py --s 20000 --o /blue/boucher/yu.hong/oneSampTest/DataSet/genePop100Ix100L > /blue/boucher/yu.hong/oneSampTest/genePop100Ix100L.out
+python /blue/boucher/suhashidesilva/ONeSAMP_3/main.py --s 20000 --o /blue/boucher/suhashidesilva/ONeSAMP_3/exampleData/genePop10Ix30L > /blue/boucher/suhashidesilva/ONeSAMP_3/genePop10Ix30L.out
 
 date
