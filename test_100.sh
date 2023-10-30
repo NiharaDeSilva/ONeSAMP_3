6f19a5c26becac7a9492d7b7d07cbf33c7070c52
Suhashi De Silva <suhashi.desilva@gmail.com>
2023-10-30 10:20:43 -0400

    output file of 200x320

--- test_100.sh
+++ test_100.sh
@@ -3,7 +3,7 @@
 #SBATCH --mail-type=ALL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=suhashi.desilva@gmail.com     # Where to send mail
 #SBATCH --ntasks=64                    # Run on a single CPU
-#SBATCH --mem=1gb                     # Job memory request
+#SBATCH --mem=30gb                     # Job memory request
 #SBATCH --time=24:00:00               # Time limit hrs:min:sec
 #SBATCH --output=serial_test_%j.log   # Standard output and error log
 pwd; hostname; date
