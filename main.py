eee01ae9e1f163c24a9866fcb0fa23c3fed3ee6f
NiharaDeSilva <suhashi.desilva@gmail.com>
2023-11-30 21:54:32 -0500

    Feedforward Neural Network

--- main.py
+++ main.py
@@ -473,3 +473,109 @@ print("--- %s seconds ---" % (time.time() - start_time))
 ##########################
 # END
 #########################
+# CNN
+#########################
+import copy
+
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import tqdm
+from sklearn.preprocessing import StandardScaler
+
+# Standardizing data
+# scaler = StandardScaler()
+# scaler.fit(X_train)
+# X_train = scaler.transform(X_train)
+# X_test = scaler.transform(X_test)
+
+# Convert to 2D PyTorch tensors
+X_train = X_train.astype(np.float32)
+X_train = torch.tensor(X_train, dtype=torch.float32)
+y_train = y_train.astype(np.float32)
+y_train = torch.tensor(y_train, dtype=torch.float32).reshape(-1, 1)
+X_test = X_test.astype(np.float32)
+X_test = torch.tensor(X_test, dtype=torch.float32)
+y_test = y_test.astype(np.float32)
+y_test = torch.tensor(y_test, dtype=torch.float32).reshape(-1, 1)
+
+# Define the model
+model = nn.Sequential(
+    nn.Linear(5, 20),
+    nn.ReLU(),
+    nn.Linear(20, 10),
+    nn.ReLU(),
+    nn.Linear(10, 5),
+    nn.ReLU(),
+    nn.Linear(5, 1)
+)
+
+# loss function and optimizer
+loss_fn = nn.MSELoss()  # mean square error
+optimizer = optim.Adam(model.parameters(), lr=0.0001)
+
+n_epochs = 100   # number of epochs to run
+batch_size = 10  # size of each batch
+batch_start = torch.arange(0, len(X_train), batch_size)
+
+# Hold the best model
+best_mse = np.inf   # init to infinity
+best_weights = None
+history = []
+
+for epoch in range(n_epochs):
+    model.train()
+    with tqdm.tqdm(batch_start, unit="batch", mininterval=0, disable=True) as bar:
+        bar.set_description(f"Epoch {epoch}")
+        for start in bar:
+            # take a batch
+            X_batch = X_train[start:start+batch_size]
+            y_batch = y_train[start:start+batch_size]
+            # forward pass
+            y_pred = model(X_batch)
+            loss = loss_fn(y_pred, y_batch)
+            # backward pass
+            optimizer.zero_grad()
+            loss.backward()
+            # update weights
+            optimizer.step()
+            # print progress
+            bar.set_postfix(mse=float(loss))
+    # evaluate accuracy at end of each epoch
+    model.eval()
+    y_pred = model(X_test)
+    mse = loss_fn(y_pred, y_test)
+    mse = float(mse)
+    history.append(mse)
+    if mse < best_mse:
+        best_mse = mse
+        best_weights = copy.deepcopy(model.state_dict())
+
+# restore model and return best accuracy
+model.load_state_dict(best_weights)
+print("MSE: %.2f" % best_mse)
+print("RMSE: %.2f" % np.sqrt(best_mse))
+# plt.plot(history)
+# plt.show()
+
+# model.eval()
+# with torch.no_grad():
+#     # Test out inference with 5 samples
+#     for i in range(5):
+#         X_sample = X_test[i: i+1]
+#         # X_sample = scaler.transform(X_sample)
+#         X_sample = torch.tensor(X_sample, dtype=torch.float32)
+#         y_pred = model(X_sample)
+#         print(f"{X_test[i]} -> {y_pred[0].numpy()} (expected {y_test[i].numpy()})")
+
+
+model.eval()  # Set the model to evaluation mode
+Z = Z.astype(np.float32)
+with torch.no_grad():
+    input_data = torch.tensor(Z, dtype=torch.float32)  # Convert new data to a PyTorch tensor
+    prediction = model(input_data)  # Forward pass to make predictions
+    predicted_value = prediction.item()  # Extract the predicted value (assuming a single output)
+
+print(f"\nPrediction of Feedforward Neural Network Model: {round(predicted_value,2)}")
+
