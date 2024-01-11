c7d34872f58738947cfebec9801cdf4d36bde5c2
suhashidesilva <suhashi.desilva@gmail.com>
2024-01-11 12:49:24 -0500

    input populations generated using msprime

---
+++ scripts/popGenerator.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+ONESAMP2COAL_MINALLELEFREQUENCY=0.05
+mutationRate="0.0012"
+rangeNe=100,500
+theta=0.000048,0.0048
+microsatsOrSNPs=s
+NeVals="00200"
+numPOP="00256"
+
+outputSampleSizes=(40 50 100 200)
+locis=(40 80 160 320 1000 2000 4000 8000)
+
+for outputSampleSize in "${outputSampleSizes[@]}"; do
+  for loci in "${locis[@]}"; do
+      ./refactor -t1 -rC -b$NeVals -d1 -u$mutationRate -v${theta} -$microsatsOrSNPs -l$loci -i$outputSampleSize -o1 -f$ONESAMP2COAL_MINALLELEFREQUENCY -p > "./data/genePop${outputSampleSize}x${loci}"
+      
+  done
+done
