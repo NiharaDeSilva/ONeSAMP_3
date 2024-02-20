4d2cc1d21a2f112bb7cb9b3488fbc3656d5535de
Suhashi De Silva <suhashi.desilva@gmail.com>
2024-02-20 16:58:51 -0500

    output files

--- test_100.sh
+++ test_100.sh
@@ -5,8 +5,9 @@
 #SBATCH --ntasks=1		      # Number of tasks
 #SBATCH --cpus-per-task=64	      # Number of cores per task
 #SBATCH --mem=50gb                     # Job memory request
-#SBATCH --time=24:00:00               # Time limit hrs:min:sec
+#SBATCH --time=04:00:00               # Time limit hrs:min:sec
 #SBATCH --output=serial_test_%j.log   # Standard output and error log
+exec 2>&1
 
 pwd; hostname; date
 
@@ -19,20 +20,24 @@ echo "Running plot script on multiple CPU cores"
 folder="/blue/boucher/suhashidesilva/ONeSAMP_3/data/datav1"
 output="/blue/boucher/suhashidesilva/ONeSAMP_3/output/V8"
 
-# Iterate through the files in the folder
-for file in "$folder"/*; do
-    if [ -f "$file" ]; then
-        filename=$(basename -- "$file")
-        filename_no_extension="${filename%.*}"
-        output_file="$output/${filename_no_extension}"
-        python /blue/boucher/suhashidesilva/ONeSAMP_3/main.py --s 20000 --o "$file" > "$output_file"
-        echo "Processed $file and saved output to $output_file"
-    fi
-done
+#Iterate through the files in the folder
+#for file in "$folder"/*; do
+#    if [ -f "$file" ]; then
+#        filename=$(basename -- "$file")
+#        filename_no_extension="${filename%.*}"
+#        output_file="$output/${filename_no_extension}"
+#        python /blue/boucher/suhashidesilva/ONeSAMP_3/main.py --s 20000 --o "$file" > "$output_file"
+#        echo "Processed $file and saved output to $output_file"
+#    fi
+#done
 
 
-#python /blue/boucher/suhashidesilva/ONeSAMP_3/main.py --s 20000 --o /blue/boucher/suhashidesilva/ONeSAMP_3/data_wm/genePop200x40 > /blue/boucher/suhashidesilva/ONeSAMP_3/output/V1/genePop200x40.out
-#python /blue/boucher/suhashidesilva/ONeSAMP_3/main.py --s 20000 --o /blue/boucher/suhashidesilva/ONeSAMP_3/data_wm/genePop100x320 > /blue/boucher/suhashidesilva/ONeSAMP_3/output/V1/genePop100x320.out
+#python /blue/boucher/suhashidesilva/ONeSAMP_3/main.py --s 20000 --o /blue/boucher/suhashidesilva/ONeSAMP_3/data/datav1/genePop50x40 > /blue/boucher/suhashidesilva/ONeSAMP_3/output/V8/genePop50x40
+python /blue/boucher/suhashidesilva/ONeSAMP_3/main.py --s 20000 --o /blue/boucher/suhashidesilva/ONeSAMP_3/data/datav1/genePop50x80 > /blue/boucher/suhashidesilva/ONeSAMP_3/output/V8/genePop50x80
+python /blue/boucher/suhashidesilva/ONeSAMP_3/main.py --s 20000 --o /blue/boucher/suhashidesilva/ONeSAMP_3/data/datav1/genePop50x160 > /blue/boucher/suhashidesilva/ONeSAMP_3/output/V8/genePop50x160
+python /blue/boucher/suhashidesilva/ONeSAMP_3/main.py --s 20000 --o /blue/boucher/suhashidesilva/ONeSAMP_3/data/datav1/genePop50x320 > /blue/boucher/suhashidesilva/ONeSAMP_3/output/V8/genePop50x320
+
+
 
 
 
